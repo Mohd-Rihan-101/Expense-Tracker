d60917db689c90fbf98022ad3974d1058f76689e
Mohd Rihan <137299852+Mohd-Rihan-101@users.noreply.github.com>
2025-06-30 01:48:22 +0530

    add routes in server

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 require('./db');
+
+dotenv.config();
+app.use(express.json());
+
+const authRoutes = require("./routes/authRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+
+app.use("/api/auth", authRoutes);
+app.use("/api/transactions", transactionRoutes);
 
 app.get('/', (req,res)=>{
     res.send("welcome to our App");
@@ -9,3 +20,6 @@ app.get('/', (req,res)=>{
 app.listen(6000, ()=>{
     console.log("Server listening on PORT 6000");
 })
+
+
+
